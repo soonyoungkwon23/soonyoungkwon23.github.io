27860912b18b8b8f84972612b4fec1e7ee1d5c98
soonyoungkwon23 <soonyoungkwon23@gmail.com>
2025-08-03 07:04:40 +0000

    Deploying to gh-pages from @ soonyoungkwon23/soonyoungkwon23.github.io@5dddd7fbaaf26375cba67e532cfd1b17c517b67f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume-cv",
+          title: "Resume/CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
