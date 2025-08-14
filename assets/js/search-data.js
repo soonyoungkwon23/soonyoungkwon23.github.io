8323136353de3a61fa34bcf19bb4f40447c5b10a
soonyoungkwon23 <soonyoungkwon23@gmail.com>
2025-08-14 14:41:13 +0000

    Deploying to gh-pages from @ soonyoungkwon23/soonyoungkwon23.github.io@1888bc1106acfde6f4518e6f765869629e08f5bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,9 +412,9 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-universal-antenna-remounting-system",
+          title: 'Universal Antenna Remounting System',
+          description: "High-precision GPS antenna mount for satellite and drone tracking",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
